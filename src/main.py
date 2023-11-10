274832951c1f6b5fe4dcac0c18bb52ca42a56d17
molok0 <molok0@protonmail.com>
2023-11-10 17:45:54 +0700

    modified

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ from db import db_main
 from cam import QR_videocapture
 import cv2
 from db.database import db
+import numpy as np
 
 key = QR_videocapture.scanQR()
 connection = db.create_connection('./db/database/ReagentFlowDB.db')
-db_main.update(connection, key, 1000)
+db_main.update(connection, key, np.random.randint(1,200))
 connection.close()
