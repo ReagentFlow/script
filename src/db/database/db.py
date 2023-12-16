ed24610513c71e9bb6c0c4e49dafefd18c7cc595
quzorr <egor.khantakov@gmail.com>
2023-12-16 12:11:57 +0700

    .

--- src/db/database/db.py
+++ src/db/database/db.py
@@ -8,7 +8,6 @@ def create_connection(path):
         print("Connection to SQLite DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
-
     return connection
 
 def execute_get(connection, query): 
